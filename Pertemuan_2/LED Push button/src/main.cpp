bc2b96a8ac50975a34b97d6446c4791d4fd69318
dadyakengrayisaeanandura <dadyakengrayisaeanandura@mail.ugm.ac.id>
2026-09-11 21:53:35 +0700

    Update code main.cpp

--- Pertemuan_2/LED Push button/src/main.cpp
+++ Pertemuan_2/LED Push button/src/main.cpp
@@ -7,23 +7,23 @@ int ledPinP = 33;
 bool buttonState = false;
 
 void setup() {
-  pinMode(ledPinM, OUTPUT);    // LED pin as an output.
+  pinMode(ledPinM, OUTPUT);
   pinMode(ledPinK, OUTPUT);
   pinMode(ledPinP, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLDOWN);  // Button pin as an input.
+  pinMode(buttonPin, INPUT_PULLDOWN);
 }
 
 void loop() {
-  // Read the state of the button
+
   buttonState = digitalRead(buttonPin);
 
-  // Check if the button is pressed
+
   if (buttonState == HIGH){
-    digitalWrite(ledPinM, HIGH);   // Turn LED on.
+    digitalWrite(ledPinM, HIGH);
     digitalWrite(ledPinK, HIGH);
     digitalWrite(ledPinP, HIGH);
   } else {
-    // Tombol dilepas: Ketiga LED mati
+
     digitalWrite(ledPinM, LOW);
     digitalWrite(ledPinK, LOW);
     digitalWrite(ledPinP, LOW);
